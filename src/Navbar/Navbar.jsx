8c63d7857a3eb8d67a31be5f9f1145538c11542f
moid1923 <mueed.octaloop@gmail.com>
2025-01-27 09:30:12 +0500

    responsive6

--- src/Navbar/Navbar.jsx
+++ src/Navbar/Navbar.jsx
@@ -5,7 +5,7 @@ import { IoClose } from "react-icons/io5";
 function Navbar() {
   const [sideMenu, setSideMenu] = useState(false);
 
-  // Toggle menu state
+  
   const toggleMenu = () =>{
     setSideMenu((prev) => !prev);
   } 
@@ -13,12 +13,12 @@ function Navbar() {
   return (
     <div className="flex items-center justify-center xs:w-full relative">
       <nav className="flex items-center justify-between px-12 py-6 md:p-6 text-black max-w-[1178px] w-full">
-        {/* Logo */}
+        
         <div className="logo">
           <p className="text-2xl">🍕</p>
         </div>
 
-        {/* Links */}
+       
         <div className="links sm:mx-48 hidden md:flex items-center justify-center">
           <ul className="flex items-center space-x-4">
             <li className="cursor-pointer">Home</li>
@@ -28,15 +28,15 @@ function Navbar() {
           </ul>
         </div>
 
-        {/* Buttons */}
-        <div className="btns flex gap-x-3 hidden md:block">
+        
+        <div className="btns  gap-x-3 hidden md:block">
           <button className="nav-btn w-28 p-1">Login</button>
           <button className="nav-btn w-[115px] py-3 rounded-full px-4 bg-primary text-base text-white">
             Sign Up
           </button>
         </div>
 
-        {/* Mobile Menu Icon */}
+        
         <button
           onClick={toggleMenu}
           className="md:hidden cursor-pointer p-2 text-gray-700"
@@ -47,14 +47,14 @@ function Navbar() {
         </button>
       </nav>
 
-      {/* Side Menu */}
+     
       {sideMenu && (
         <div className="fixed inset-0 bg-black/50 z-20" onClick={toggleMenu}>
           <div
             className="absolute top-0 right-0 w-64 h-full bg-white shadow-md z-30"
-            // Prevent closing when clicking inside menu
+            
           >
-            {/* Menu Items */}
+            
             <ul className="flex flex-col items-start p-4 space-y-3">
               <li className="cursor-pointer">Home</li>
               <li className="cursor-pointer">About</li>
